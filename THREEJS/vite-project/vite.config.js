1df6de1a343be7f056acfebad7a948eac1c71603
Eric <eric.ruiz@enti.cat>
2024-11-01 13:15:37 +0100

    It no longer explodes

--- THREEJS/vite-project/vite.config.js
+++ THREEJS/vite-project/vite.config.js
@@ -1,8 +1,15 @@
 import vitePluginString from 'vite-plugin-string';
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default {
   plugins: [
     vitePluginString(),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`
+    })
   ],
   build: {
     target: 'esnext', // Enables support for top-level await and latest JavaScript features
