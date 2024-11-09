017f0773b64b845aa3714f114623edb1ad99046e
Eric <eric.ruiz@enti.cat>
2024-11-09 21:59:38 +0100

    hotfix

--- THREEJS/vite-project/project/shaders/ScrabbleFragment.glsl
+++ THREEJS/vite-project/project/shaders/ScrabbleFragment.glsl
@@ -1,8 +1,15 @@
 uniform sampler2D myTexture;
 varying vec2 vUV;
 
+uniform float uTime;
+
+uniform float displacementScale;
+uniform float displacementStrength;
+
 void main()
 {
-    vec4 texColor = texture(myTexture, vUV);
+    vec2 displacedUV = vUV + ((gln_perlin(vUV * displacementScale + vec2(uTime)) * displacementStrength));// - displacementStrength / 2.0);
+    vec4 texColor = texture(myTexture, displacedUV);
+
     csm_DiffuseColor = texColor;
 }
