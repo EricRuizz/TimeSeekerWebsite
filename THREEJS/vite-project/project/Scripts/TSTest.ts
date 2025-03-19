9b57abaf640711b16dc1213a23e314d61387287c
Eric <eric.ruiz@enti.cat>
2025-03-19 13:54:25 +0100

    Typescript, GSAP, and full rework

---
+++ THREEJS/vite-project/project/Scripts/TSTest.ts
@@ -0,0 +1,18 @@
+import '/style.css';
+import * as THREE from 'three';
+
+import { gsap } from "gsap";
+    
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { Draggable } from "gsap/Draggable";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { EaselPlugin } from "gsap/EaselPlugin";
+import { PixiPlugin } from "gsap/PixiPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+
+
+
+
