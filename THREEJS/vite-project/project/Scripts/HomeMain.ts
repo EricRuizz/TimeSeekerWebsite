af724b931755d592ee3250363ee9cc258359bc18
Eric <eric.ruiz@enti.cat>
2025-03-21 12:24:23 +0100

    page selector js start

--- THREEJS/vite-project/project/Scripts/HomeMain.ts
+++ THREEJS/vite-project/project/Scripts/HomeMain.ts
@@ -15,29 +15,107 @@ import { TextPlugin } from "gsap/TextPlugin";
 
 
 
+enum PageSelectorItemType {
+    Games = 0,
+    Art = 1,
+    Music = 2,
+    Others = 3,
+}
+
+var isPreviewCardShown = false as boolean;
+var isHoveringPageSelectorItem = false as boolean;
+
+var previewCard: HTMLElement | null;
+var previewCardStripeTop: HTMLElement | null;
+var previewCardStripeBot: HTMLElement | null;
+
+var pageSelectorContentGames: HTMLElement | null;
+var pageSelectorContentArt: HTMLElement | null;
+var pageSelectorContentMusic: HTMLElement | null;
+var pageSelectorContentOthers: HTMLElement | null;
+
 document.addEventListener("DOMContentLoaded", () => {
-    const previewCard = document.querySelector(".previewCard") as HTMLElement | null;
-    const previewCardStripe = document.querySelector(".previewCardStripe") as HTMLElement | null;
-    const pageSelectorContent = document.querySelector(".pageSelectorContent") as HTMLElement | null;
-  
-    if (previewCard && previewCardStripe && pageSelectorContent)
-    {
-        pageSelectorContent.addEventListener("mouseenter", () => {
-            //TODO - previewCard.classList.add("active");
-            //TODO - previewCardStripe.classList.add("active");
+    //Event class
+    pageSelectorContentGames = document.querySelector(".pageSelectorContent.games");
+    pageSelectorContentArt = document.querySelector(".pageSelectorContent.art");
+    pageSelectorContentMusic = document.querySelector(".pageSelectorContent.music");
+    pageSelectorContentOthers = document.querySelector(".pageSelectorContent.others");
 
-            //TODO - Search if I can simply call a function instead of "add" and "remove"
-        });
-  
-        pageSelectorContent.addEventListener("mouseleave", () => {
-            //TODO - previewCard.classList.remove("active");
-            //TODO - previewCardStripe.classList.remove("active");
+    pageSelectorContentGames?.addEventListener("mouseenter", () => HoveredPageSelectorItem(PageSelectorItemType.Games));
+    pageSelectorContentArt?.addEventListener("mouseenter", () => HoveredPageSelectorItem(PageSelectorItemType.Art));
+    pageSelectorContentMusic?.addEventListener("mouseenter", () => HoveredPageSelectorItem(PageSelectorItemType.Music));
+    pageSelectorContentOthers?.addEventListener("mouseenter", () => HoveredPageSelectorItem(PageSelectorItemType.Others));
 
-            //TODO - here add the "deactivate" function?
-        });
+    pageSelectorContentGames?.addEventListener("mouseleave", () => UnhoveredPageSelectorItem());
+    pageSelectorContentArt?.addEventListener("mouseleave", () => UnhoveredPageSelectorItem());
+    pageSelectorContentMusic?.addEventListener("mouseleave", () => UnhoveredPageSelectorItem());
+    pageSelectorContentOthers?.addEventListener("mouseleave", () => UnhoveredPageSelectorItem());
 
-    } else {
-        console.log("CSS Elements not found");
-    }
+    //Affected class
+    previewCard = document.querySelector(".previewCard");
+    previewCardStripeTop = document.querySelector(".previewCardStripe.top");
+    previewCardStripeBot = document.querySelector(".previewCardStripe.bot");
 });
-  
+
+// HOVER PAGE SELECTOR ITEM
+function HoveredPageSelectorItem(type: PageSelectorItemType)
+{
+    console.log("HoveredPageSelectorItem");
+    isHoveringPageSelectorItem = true;
+
+    if(isPreviewCardShown == false)
+    {
+        ShowPreviewCard();
+    }
+    HoveredPageSelectorItemType(type);
+}
+
+function ShowPreviewCard()
+{
+    isPreviewCardShown = true;
+
+    //TODO - previewCard?.classList.add("TEST");
+}
+
+function HoveredPageSelectorItemType(type: PageSelectorItemType)
+{
+    if(type == PageSelectorItemType.Games)
+    {
+
+    }
+    else if(type == PageSelectorItemType.Art)
+    {
+
+    }
+    else if(type == PageSelectorItemType.Music)
+    {
+
+    }
+    else if(type == PageSelectorItemType.Others)
+    {
+
+    }
+}
+
+// UNHOVER PAGE SELECTOR ITEM
+function UnhoveredPageSelectorItem()
+{
+    console.log("UnhoveredPageSelectorItem");
+    isHoveringPageSelectorItem = false;
+
+    setTimeout(() =>
+    {
+        if(isPreviewCardShown && !isHoveringPageSelectorItem)
+        {
+            HidePreviewCard();
+        }
+    }, 100);
+}
+
+function HidePreviewCard()
+{
+    
+}
+
+//TODO - if "mouseleave" for 0.1 seconds, then make the preview card disappear
+//TODO - only call previewCard.appear if previewCard is hiden 
