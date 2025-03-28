7099bc95257545cdde41d381450cd2c986506107
Eric <eric.ruiz@enti.cat>
2025-03-28 20:39:13 +0100

    Rework fully done, better selectors and some window resizing

--- THREEJS/vite-project/project/Scripts/GamesMain.ts
+++ THREEJS/vite-project/project/Scripts/GamesMain.ts
@@ -2,11 +2,6 @@ import '/style.css';
 import '/project/CSS/GamesStyle.css';
 
 import { gsap } from "gsap";
-import { HelixCurve } from 'three/examples/jsm/curves/CurveExtras';
-//import { ScrollTrigger } from "gsap/ScrollTrigger";
-//import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-
-//gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 history.replaceState({}, "", "/Games");
 
@@ -15,11 +10,7 @@ history.replaceState({}, "", "/Games");
 document.addEventListener("DOMContentLoaded", () =>
 {
     document.querySelectorAll(".cell").forEach(cell =>
-    {
-        const contentHeight = "80%" as string;
-        const selectorHeight = "38.5vh" as string;
-        const selectorWidth = "1vw" as string;
-        
+    {        
         //SYNTAX - d(uration)_name
         const d_bannerAnim = 0.15 as number;
         const d_sHeight = 0.15 as number;
@@ -27,19 +18,28 @@ document.addEventListener("DOMContentLoaded", () =>
 
         const content = cell.querySelector(".content") || cell;
         const hover = cell.querySelector(".selector");
-        const hSelectors: (Element | null | undefined)[] = [...hover?.querySelectorAll(".selector-bar") || []];
+        const selectorBars: (Element | null | undefined)[] = [...hover?.querySelectorAll(".selector-bar") || []];
         const previewGIF = content.querySelector(".preview");
-        const hBanner = hover?.querySelector(".selector-banner");
+        const selectorBanner = hover?.querySelector(".selector-banner");
 
-        const contentHeightA = window.getComputedStyle(content).getPropertyValue("height") as string;
+        const cellHeight = window.getComputedStyle(cell).getPropertyValue("height") as string;
+        const cellWidth = window.getComputedStyle(cell).getPropertyValue("width") as string;
+        const contentHeight = window.getComputedStyle(content).getPropertyValue("height") as string;
+
+        const selectorBarHeightCoef = 1.05 as number;
+        const selectorBarWidthCoef = 0.05 as number;
+        const selectorBarHeight = Number(cellHeight.substring(0, cellHeight.length - 2)) * selectorBarHeightCoef;
+        const selectorBarWidth = Number(cellWidth.substring(0, cellWidth.length - 2)) * selectorBarWidthCoef;
+
+        gsap.set(selectorBars, { height: 0, width: 0 });
 
         cell.addEventListener("mouseenter", () =>
         {
-            gsap.set(hSelectors, { opacity: 1 });
+            gsap.set(selectorBars, { opacity: 1 });
             var tl = gsap.timeline();
-            tl.to(hSelectors, { height: selectorHeight, duration: d_sHeight }, 0);
-            tl.to(hSelectors, { width: selectorWidth, duration: d_sWidth }, ">");
-            if (hBanner) tl.to(hBanner, { height: "20vh", duration: d_bannerAnim }, 0);
+            tl.to(selectorBars, { height: selectorBarHeight, duration: d_sHeight }, 0);
+            tl.to(selectorBars, { width: selectorBarWidth, duration: d_sWidth }, ">");
+            if (selectorBanner) tl.to(selectorBanner, { height: cellHeight, duration: d_bannerAnim }, 0);
             tl.to(previewGIF, { opacity: 1, duration: d_bannerAnim }, 0);
             tl.to(content, { height: "100%", duration: d_bannerAnim }, 0);
         });
@@ -47,12 +47,12 @@ document.addEventListener("DOMContentLoaded", () =>
         cell.addEventListener("mouseleave", () =>
         {            
             var tl = gsap.timeline();
-            tl.to(hSelectors, { width: "0%", duration: d_sWidth }, 0);
-            tl.to(hSelectors, { height: "0%", duration: d_sHeight }, ">");
-            tl.set(hSelectors, { opacity: 0 }, ">");
-            if (hBanner) tl.to(hBanner, { height: "0vh", duration: d_bannerAnim }, 0);
+            tl.to(selectorBars, { width: "0%", duration: d_sWidth }, 0);
+            tl.to(selectorBars, { height: "0%", duration: d_sHeight }, ">");
+            tl.set(selectorBars, { opacity: 0 }, ">");
+            if (selectorBanner) tl.to(selectorBanner, { height: "0vh", duration: d_bannerAnim }, 0);
             tl.to(previewGIF, { opacity: 0, duration: d_bannerAnim }, 0);
-            tl.to(content, { height: contentHeightA, duration: d_bannerAnim }, 0);
+            tl.to(content, { height: contentHeight, duration: d_bannerAnim }, 0);
         });
     });    
 });
