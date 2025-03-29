8b07672d0a763d03947203f86ff801cfcc2142e6
Eric <eric.ruiz@enti.cat>
2025-03-29 21:22:16 +0100

    Resizing fully supported

--- THREEJS/vite-project/project/Scripts/GamesMain.ts
+++ THREEJS/vite-project/project/Scripts/GamesMain.ts
@@ -7,52 +7,100 @@ history.replaceState({}, "", "/Games");
 
 
 
+class GridCell
+{
+    private cell: Element
+    private content: Element;
+    private hover: Element | null;
+    private selectorBars: (Element | null)[];
+    private previewGIF: Element | null;
+    private selectorBanner: Element | null;
+    
+    private cellHeight: string;
+    private cellWidth: string;
+    
+    private selectorBarHeight: number;
+    private selectorBarWidth: number;
+
+    private mouseEnterTL: gsap.core.Timeline;
+
+    constructor(cell: Element)
+    {
+        this.cell = cell;
+        this.content = cell.querySelector(".content") || cell;
+        this.hover = cell.querySelector(".selector");
+        this.selectorBars = [...this.hover?.querySelectorAll(".selector-bar") || []];
+        this.previewGIF = this.content.querySelector(".preview");
+        this.selectorBanner = this.hover?.querySelector(".selector-banner") || cell;
+
+        // # === Resizing relevant variables === #
+        this.cellHeight = "";
+        this.cellWidth = "";
+        
+        this.selectorBarHeight = 0;
+        this.selectorBarWidth = 0;
+
+        this.mouseEnterTL = gsap.timeline();
+        // # === Resizing relevant variables === #
+
+        this.InitResizingRelevantVariables();
+
+        window.addEventListener("resize", () => this.OnWindowResize());
+    }
+
+    public InitResizingRelevantVariables(): void
+    {
+        this.cellHeight = window.getComputedStyle(this.cell).getPropertyValue("height") as string;
+        this.cellWidth = window.getComputedStyle(this.cell).getPropertyValue("width") as string;
+        
+        this.selectorBarHeight = Number(this.cellHeight.substring(0, this.cellHeight.length - 2)) * selectorBarHeightCoef;
+        this.selectorBarWidth = Number(this.cellWidth.substring(0, this.cellWidth.length - 2)) * selectorBarWidthCoef;
+
+        this.mouseEnterTL = gsap.timeline({ paused: true })
+        .set(this.selectorBars, { opacity: 1 })
+        .to(this.selectorBars, { height: this.selectorBarHeight, duration: d_sHeight }, 0)
+        .to(this.selectorBars, { width: this.selectorBarWidth, duration: d_sWidth }, ">")
+        .to(this.selectorBanner, { height: this.cellHeight, duration: d_bannerAnim }, 0)
+        .to(this.previewGIF, { opacity: 1, duration: d_bannerAnim }, 0)
+        .to(this.content, { height: "100%", duration: d_bannerAnim }, 0);
+    }
+
+    public Init()
+    {
+        this.cell.addEventListener("mouseenter", () => this.MouseEnter());
+        this.cell.addEventListener("mouseleave", () => this.MouseLeave());
+    }
+
+    private MouseEnter()
+    {
+        this.mouseEnterTL.restart();
+    }
+
+    private MouseLeave()
+    {
+        this.mouseEnterTL.reverse();
+    }
+
+    private OnWindowResize()
+    {
+        this.InitResizingRelevantVariables();
+    }
+}
+
+var gridCells: GridCell[];
+
+//SYNTAX - d(uration)_name
+const d_bannerAnim = 0.15 as number;
+const d_sHeight = 0.15 as number;
+const d_sWidth = 0.05 as number;
+
+const selectorBarHeightCoef = 1.05 as number;
+const selectorBarWidthCoef = 0.05 as number;
+
+
 document.addEventListener("DOMContentLoaded", () =>
 {
-    document.querySelectorAll(".cell").forEach(cell =>
-    {        
-        //SYNTAX - d(uration)_name
-        const d_bannerAnim = 0.15 as number;
-        const d_sHeight = 0.15 as number;
-        const d_sWidth = 0.05 as number;
+    gridCells = [...document.querySelectorAll(".cell")].map(cellElement => new GridCell(cellElement));
 
-        const content = cell.querySelector(".content") || cell;
-        const hover = cell.querySelector(".selector");
-        const selectorBars: (Element | null | undefined)[] = [...hover?.querySelectorAll(".selector-bar") || []];
-        const previewGIF = content.querySelector(".preview");
-        const selectorBanner = hover?.querySelector(".selector-banner");
-
-        const cellHeight = window.getComputedStyle(cell).getPropertyValue("height") as string;
-        const cellWidth = window.getComputedStyle(cell).getPropertyValue("width") as string;
-        const contentHeight = window.getComputedStyle(content).getPropertyValue("height") as string;
-
-        const selectorBarHeightCoef = 1.05 as number;
-        const selectorBarWidthCoef = 0.05 as number;
-        const selectorBarHeight = Number(cellHeight.substring(0, cellHeight.length - 2)) * selectorBarHeightCoef;
-        const selectorBarWidth = Number(cellWidth.substring(0, cellWidth.length - 2)) * selectorBarWidthCoef;
-
-        gsap.set(selectorBars, { height: 0, width: 0 });
-
-        cell.addEventListener("mouseenter", () =>
-        {
-            gsap.set(selectorBars, { opacity: 1 });
-            var tl = gsap.timeline();
-            tl.to(selectorBars, { height: selectorBarHeight, duration: d_sHeight }, 0);
-            tl.to(selectorBars, { width: selectorBarWidth, duration: d_sWidth }, ">");
-            if (selectorBanner) tl.to(selectorBanner, { height: cellHeight, duration: d_bannerAnim }, 0);
-            tl.to(previewGIF, { opacity: 1, duration: d_bannerAnim }, 0);
-            tl.to(content, { height: "100%", duration: d_bannerAnim }, 0);
-        });
-    
-        cell.addEventListener("mouseleave", () =>
-        {            
-            var tl = gsap.timeline();
-            tl.to(selectorBars, { width: "0%", duration: d_sWidth }, 0);
-            tl.to(selectorBars, { height: "0%", duration: d_sHeight }, ">");
-            tl.set(selectorBars, { opacity: 0 }, ">");
-            if (selectorBanner) tl.to(selectorBanner, { height: "0vh", duration: d_bannerAnim }, 0);
-            tl.to(previewGIF, { opacity: 0, duration: d_bannerAnim }, 0);
-            tl.to(content, { height: contentHeight, duration: d_bannerAnim }, 0);
-        });
-    });    
+    gridCells.forEach(gridCell => { gridCell.Init() });
 });
