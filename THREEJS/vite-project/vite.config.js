b3f0b4c2c09150a86a304880f233b9c53ef3abf4
Eric <eric.ruiz@enti.cat>
2025-04-14 13:20:59 +0200

    gh-pages installation and setup

--- THREEJS/vite-project/vite.config.js
+++ THREEJS/vite-project/vite.config.js
@@ -5,13 +5,12 @@ export default {
   plugins: [
     vitePluginString(),
     topLevelAwait({
-      // The export name of top-level await promise for each chunk module
       promiseExportName: "__tla",
-      // The function to generate import names of top-level await promise in each chunk module
       promiseImportName: i => `__tla_${i}`
     })
   ],
   build: {
-    target: 'esnext', // Enables support for top-level await and latest JavaScript features
+    target: 'esnext',
   },
+  base: "TimeSeekerWebsite",
 };
