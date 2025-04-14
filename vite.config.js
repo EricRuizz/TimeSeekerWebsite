364db1f68950f8388ded0f038954b4a8ffc67f5d
Eric <eric.ruiz@enti.cat>
2025-04-15 00:23:35 +0200

    HTML test

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import vitePluginString from 'vite-plugin-string';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
@@ -12,5 +13,14 @@ export default defineConfig({
   ],
   build: {
     target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        games: resolve(__dirname, 'project/HTML/Games.html'),
+        art: resolve(__dirname, 'project/HTML/Art.html'),
+        music: resolve(__dirname, 'project/HTML/Music.html'),
+        others: resolve(__dirname, 'project/HTML/Others.html'),
+      }
+    }
   }
 });
